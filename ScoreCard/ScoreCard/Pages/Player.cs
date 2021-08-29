2261f44ac266ad8fcb939c351afedcf307576741
Hans Voll <hvoll44@gmail.com>
2021-08-29 17:56:40 -0400

    First Commit

---
+++ ScoreCard/ScoreCard/Pages/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ScoreCard.Pages
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public List<int> Scores { get; set; } = new List<int>();
+
+        public Player(string name, int rounds)
+        {
+            Name = name;
+
+            for (int i = 0; i < rounds; i++)
+            {
+                Scores.Add(0);
+            }
+        }
+
+        public Player()
+        {
+        }
+    }
+}
---
+++ ScoreCard/ScoreCard/Pages/ScoreSheet.razor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Text.Json;
+
+namespace ScoreCard.Pages
+{
+    public partial class ScoreSheet
+    {
+        private List<Player> _players = new List<Player>();
+
+        [Parameter]
+        public int Players { get; set; } = 3;
+
+        [Parameter]
+        public int Rounds { get; set; } = 7;
+
+        protected override void OnParametersSet()
+        {
+            for (int i = 0; i < Players; i++)
+            {
+                _players.Add(new Player($"Player{i + 1}", Rounds));
+            }
+        }
+
+        public async Task Save()
+        {
+            var json = JsonSerializer.Serialize(_players);
+            await JSRuntime.InvokeVoidAsync("localStorage.setItem", "name", json);
+        }
+
+        public async Task Read()
+        {
+            var json = await JSRuntime.InvokeAsync<string>("localStorage.getItem", "name");
+            _players = JsonSerializer.Deserialize<List<Player>>(json);
+        }
+    }
+}
